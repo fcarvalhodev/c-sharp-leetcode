60e3e6885235fd2007eaf3a96ad4ebc2ce747318
Fabio Carvalho <fabio.carvalllho@live.com>
2022-09-18 21:56:36 -0300

    Include FruitsIntoBasket problem

---
+++ FruitsIntoBasketsProblem/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FruitsIntoBasketsProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fruits = new int[] { 1, 2, 1 };
+            int result = TotalFruit(fruits);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        public static int TotalFruit(int[] fruits)
+        {
+            if (fruits == null)
+                return 0;
+
+            int lastFruit = -1;
+            int secondLastFruit = -1;
+            int lastFruitCount = -1;
+            int currentMax = 1;
+            int max = 0;
+
+            // traverse whole tree 
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                // if current fruit is among the last two fruit increase currentMax
+                if ((fruits[i] == lastFruit) || (fruits[i] == secondLastFruit))
+                {
+                    currentMax++;
+                }
+                else
+                {
+                    // if current fruit is not among the last two fruit
+                    //then secondLastFruitCount becomes lastFruitCount and lastFruitCount becomes 1
+                    if (lastFruitCount != -1)
+                        currentMax = lastFruitCount + 1;
+                }
+
+                if (fruits[i] != lastFruit)
+                {
+                    // keep updating the last two fruits  
+                    secondLastFruit = lastFruit;
+                    lastFruit = fruits[i];
+                    lastFruitCount = 1;
+                }
+                else
+                {
+                    //if still last fruit is same increase the count 
+                    lastFruitCount++;
+                }
+
+                max = Math.Max(max, currentMax);
+            }
+
+            return max;
+        }
+    }
+}
+
+
+// 1 - Percorrer cada arvore e verificar o tipo.
+// 2 - Para cada tipo encontrado eu armazeno em uma cesta.
+// 3 - Posso colher a quantidade máxima que der, mas respeitando apenas 1 tipo para cada cesta.
+// 4 - Preciso validar a quantidade de cada tipo para ter certeza que eu colhar o máximo possível.
+// 5 - Com a lista montada, ordenar pelo valor e somar os valores dos dois primeiros resultados.
+
+//int result = 0;
+//Dictionary<int, int> basket = new Dictionary<int, int>();
+
+//for (int i = 0; i < fruits.Length; i++)
+//{
+//    int typeCurrValue = 0;
+//    if (!basket.ContainsKey(fruits[i]))
+//    {
+//        basket.Add(fruits[i], typeCurrValue + 1);
+//    }
+//    else
+//    {
+//        basket[fruits[i]] = (int)basket[fruits[i]] + 1;
+//    }
+//}
+
+////int temp;
+
+//for (int i = 0; i < basket.Count - 1; i++)
+//{
+//    // traverse i+1 to array length
+//    for (int j = i + 1; j < basket.Count; j++)
+//    {
+//        // compare array element with
+//        // all next element
+//        var elementI = basket.ElementAt(i);
+//        var elementJ = basket.ElementAt(j);
+
+//        if (elementI.Value < elementJ.Value)
+//        {
+
+//            basket.Remove(elementI.Key);
+//            basket.Remove(elementJ.Key);
+//            basket.Add(elementI.Key, elementI.Value);
+//            basket.Add(elementJ.Key, elementJ.Value);
+
+//        }
+//    }
+//}
+
+//result = basket.ElementAt(0).Value + basket.ElementAt(1).Value;
+//return result;
+
+
+
---
+++ FruitsIntoBasketsProblem/README.md
@@ -0,0 +1,23 @@
+﻿Problema: Cesto de Frutas
+
+Você está visitando uma fazendo que tem um único corredor de árvores e frutas da esquerda para a direita. As árvores são representadas por array de frutas, onde frutas[i] é o tipo da fruta que a árvore iº produz.
+
+Você irá coletar o quanto de fruta for possível. Entretanto, o dono da fazenda possui regras bem restritivas que você precisa seguir.
+
+Você pode ter apenas duas cestas, e em cada cesta você só pode ter um tipo de fruta. Não existe limite na quantidade de fruta em que cada cesto pode segurar.
+Começando de qualquer árvore, você precisa escolher exatamente uma fruta de cada árvore (incluindo a árvore inicial), enquanto se move para a direita. 
+Ao atingir uma árvore em que a fruta não pode ficar na cesta, você precisa parar.
+
+Dado a um inteiro de frutas, retorne o máximo número de frutas que você pegar.
+
+Exemplo:1
+Input: frutas = [1,2,1]
+Output: 3
+Explicação: Você pode colher de todas as três árvores.
+
+Exemplo:2
+Input: frutas = [1,2,3,2,2]
+Output: 4
+Explicação: Nós podemos pegar das árvores [2,3,2,2]. Se tivéssemos começado com a primeira árvore, nós teríamos pegado apenas das árvores [1,2].
+
+
