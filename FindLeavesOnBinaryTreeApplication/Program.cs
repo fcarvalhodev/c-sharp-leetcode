fa2ed7749b698067f20775a43cedb5a083118a0e
Fabio Carvalho <fabio.carvalllho@live.com>
2022-10-17 15:44:39 -0300

    Adição do problema FindLeaves

---
+++ FindLeavesOnBinaryTreeApplication/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindLeavesOnBinaryTreeApplication
+{
+    class Program
+    {
+        public static Dictionary<int, List<int>> nodeMap = new Dictionary<int, List<int>>();
+
+        static void Main(string[] args)
+        {
+            TreeNode lNode = new TreeNode(2, new TreeNode(4), new TreeNode(5));
+            TreeNode rNode = new TreeNode(3);
+            TreeNode root = new TreeNode(1, lNode, rNode);
+
+            var result = FindLeaves(root);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine("------Nodes removed -----");
+                foreach (var node in item)
+                {
+                    Console.WriteLine(node);
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private static int Helper(TreeNode node, IDictionary<TreeNode, int> node2LeafLevel)
+        {
+            if (node == null)
+            {
+                return -1;
+            }
+
+            if (node.left == null && node.right == null)
+            {
+                node2LeafLevel[node] = 0;
+                return 0;
+            }
+
+            var left = Helper(node.left, node2LeafLevel);
+            var right = Helper(node.right, node2LeafLevel);
+            int order = Math.Max(left, right) + 1;
+            node2LeafLevel[node] = order;
+            return order;
+        }
+
+        public static IList<IList<int>> FindLeaves(TreeNode root)
+        {
+            if (root == null)
+            {
+                return new List<IList<int>>();
+            }
+
+            IDictionary<TreeNode, int> node2LeafLevel = new Dictionary<TreeNode, int>();
+            Helper(root, node2LeafLevel);
+            int max = node2LeafLevel.Values.Max();
+            IList<IList<int>> res = new List<IList<int>>(max + 1);
+
+            for (int i = 0; i <= max; i++)
+            {
+                res.Add(new List<int>());
+            }
+
+            foreach (var p in node2LeafLevel)
+            {
+                res[p.Value].Add(p.Key.val);
+            }
+
+            return res;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ TextJustificationApplication/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextJustificationApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = FullJustify(new string[] { "This", "is", "an", "example", "of", "text", "justification." }, 16);
+            foreach (var word in result)
+            {
+                Console.WriteLine(word);
+            }
+            Console.ReadKey();
+        }
+
+        public static IList<string> FullJustify(string[] words,
+            int maxWidth)
+        {
+            IList<string> result = new List<string>();
+            int left = 0;
+
+            while (left < words.Length)
+            {
+                int right = FindRight(left, words.ToList(), maxWidth);
+                result.Add(Justfiy(left, right, words, maxWidth));
+                left = right + 1;
+            }
+
+            return result;
+        }
+
+        public static int FindRight(int left, List<string> words, int maxWidth)
+        {
+            int right = left;
+            int currentWordSize = words[right].Length;
+            right++;
+
+            while (right < words.Count && (currentWordSize + 1 + words[right].Length) <= maxWidth)
+            {
+                currentWordSize += 1 + words[right].Length;
+                right++;
+            }
+
+            return right - 1;
+        }
+
+        public static string Justfiy(int left, int right, string[] words, int maxWidth)
+        {
+            if (right - left == 0)
+            {
+                return PadResult(words[left], maxWidth);
+            }
+
+            bool isLastLine = (right == words.Length - 1);
+            int numSpaces = right - left;
+            int totalSpace = maxWidth - WordsLength(left, right, words);
+
+            string space = isLastLine ? " " : Blank(totalSpace / numSpaces);
+            int remainder = isLastLine ? 0 : totalSpace % numSpaces;
+
+            StringBuilder result = new StringBuilder();
+            for (int i = left; i <= right; i++)
+            {
+                result.Append(words[i])
+                    .Append(space)
+                    .Append(remainder-- > 0 ? " " : "");
+            }
+
+            return PadResult(result.ToString().Trim(), maxWidth);
+        }
+
+        public static int WordsLength(int left, int right, string[] words)
+        {
+            int wordsLength = 0;
+            for (int i = left; i <= right; i++)
+            {
+                wordsLength += words[i].Length;
+            }
+            return wordsLength;
+        }
+
+        public static string PadResult(string result, int maxWidth)
+        {
+            return result + Blank(maxWidth - result.Length);
+        }
+
+        public static string Blank(int length)
+        {
+            return new string(new char[length]).Replace('\0', ' ');
+        }
+    }
+}
